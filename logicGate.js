43702d7a5aac13262ad98dbbbb9fe9fd669f89df
José Vílchez <jjva90@gmail.com>
2021-04-11 20:35:15 -0500

    le logicGates

--- logicGate.js
+++ logicGate.js
@@ -12,20 +12,37 @@
  */
 
 const logicGate = (gate, signalOne, signalTwo = null) => {
+  const allowed = [0, 1];
+
+  if (allowed.indexOf(signalOne) === -1)
+    throw new Error("El primer número debe ser 0 ó 1");
+  const checkSecond = () => {
+    if (allowedValues.indexOf(signalTwo) === -1)
+      throw new Error("El segundo número debe ser 0 ó 1");
+  };
+
   if (gate === "AND") {
+    checkSecond();
     return signalOne && signalTwo ? 1 : 0;
   } else if (gate === "OR") {
+    checkSecond();
     return signalOne || signalTwo ? 1 : 0;
   } else if (gate === "NOT") {
     return Number(!signalOne);
   } else if (gate === "NAND") {
+    checkSecond();
     return signalOne && signalTwo ? 0 : 1;
   } else if (gate === "NOR") {
+    checkSecond();
     return !signalOne && !signalTwo ? 1 : 0;
   } else if (gate === "XOR") {
+    checkSecond();
     return signalOne !== signalTwo ? 1 : 0;
   } else if (gate === "XNOR") {
+    checkSecond();
     return signalOne === signalTwo ? 1 : 0;
+  } else {
+    return "Puerta no reconocida";
   }
 };
 
